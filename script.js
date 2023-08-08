1facbc3373f0d8ee5dcf837d191ef4f6d37d75f6
jwaddell10 <jewaddell91@gmail.com>
2023-08-07 20:18:47 -0500

    finished initial styling

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-let searchQuery = '';
+/* eslint-disable no-console */
+const recipeSearchBar = document.querySelector('#search');
+const searchResultDiv = document.querySelector
+
+const searchQuery = '';
 const APP_ID = '07219864';
 const APP_key = 'f83320eb349bdebfd72d4be76b29e58f';
+
+
