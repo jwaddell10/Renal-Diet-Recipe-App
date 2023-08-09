cb9dd7eb64bc913326379d5b57dfcb482202c195
jwaddell10 <jewaddell91@gmail.com>
2023-08-08 21:42:21 -0500

    update readme

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 /* eslint-disable no-console */
-const recipeSearchBar = document.querySelector('#search');
-const searchResultDiv = document.querySelector
+const recipeSearchBarForm = document.querySelector('form');
+const searchResultDiv = document.querySelector('.searchresultscontainer');
 
-const searchQuery = '';
+let searchQuery = '';
 const APP_ID = '07219864';
 const APP_key = 'f83320eb349bdebfd72d4be76b29e58f';
+const baseURL = `https://api.edamam.com/api/recipes/v2?q=pizza&app_id=${APP_ID}&app_key${APP_key}`;
 
+console.log(baseURL);
 
+recipeSearchBarForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector('input').value;
+  console.log(searchQuery);
+});
