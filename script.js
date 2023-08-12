fe24b88448d05b80fb94a95a9088b392e22f2ace
jwaddell10 <jewaddell91@gmail.com>
2023-08-12 16:05:46 -0500

    api fetch function, add preptime to html

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ const searchResultDiv = document.querySelector('.searchresultscontainer');
 
 let searchQuery = '';
 const APP_ID = '07219864';
+// eslint-disable-next-line camelcase
 const APP_key = 'f83320eb349bdebfd72d4be76b29e58f';
-const baseURL = `https://api.edamam.com/api/recipes/v2?q=pizza&app_id=${APP_ID}&app_key${APP_key}`;
-
-console.log(baseURL);
 
 recipeSearchBarForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  const img = document.querySelector('img');
   searchQuery = e.target.querySelector('input').value;
-  console.log(searchQuery);
+  fetchAPI();
+  // console.log(searchQuery);
 });
+
+async function fetchAPI() {
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=pizza&app_id=${APP_ID}&app_key=${APP_key}&health=kidney-friendly`, { mode: 'cors' });
+
+  const recipeSearchData = await response.json();
+  console.log(recipeSearchData.hits[0].recipe.url);
+}
