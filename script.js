c45aa80e1100361116b2c34c1ab0cb1a26c4d3d1
jwaddell10 <jewaddell91@gmail.com>
2023-08-16 16:29:59 -0500

    unique function depending on userfilterchoice

--- script.js
+++ script.js
@@ -20,36 +20,47 @@ recipeSearchBarForm.addEventListener('submit', (e) => {
       fetchAPIBreakfastLowPrep();
       break;
     case 'breakfast|10to20minutes':
+      fetchAPIBreakfastMediumPrep();
       console.log('this is a medium breakfast recipe');
       break;
     case 'breakfast|20plusminutes':
+      fetchAPIBreakfastHighPrep();
       console.log('this is a long breakfast recipe');
       break;
     case 'lunch|0to10minutes':
+      fetchAPILunchLowPrep();
       console.log('this is a short lunch recipe');
       break;
     case 'lunch|10to20minutes':
+      fetchAPILunchMediumPrep();
       console.log('this is a medium lunch recipe');
       break;
     case 'lunch|20plusminutes':
+      fetchAPILunchHighPrep();
       console.log('this is a long lunch recipe');
       break;
     case 'dinner|0to10minutes':
+      fetchAPIDinnerLowPrep();
       console.log('this is a short dinner recipe');
       break;
     case 'dinner|10to20minutes':
+      fetchAPIDinnerMediumPrep();
       console.log('this is a medium dinner recipe');
       break;
     case 'dinner|20plusminutes':
+      fetchAPIDinnerHighPrep();
       console.log('this is a long dinner recipe');
       break;
     case 'snack|0to10minutes':
+      fetchAPISnackLowPrep();
       console.log('this is a short snack recipe');
       break;
     case 'snack|10to20minutes':
+      fetchAPISnackMediumPrep();
       console.log('this is a medium snack recipe');
       break;
     case 'snack|20plusminutes':
+      fetchAPISnackHighPrep();
       console.log('this is a long snack recipe');
       break;
   }
@@ -59,8 +70,118 @@ recipeSearchBarForm.addEventListener('submit', (e) => {
 
 async function fetchAPIBreakfastLowPrep() {
   console.log(searchQuery);
-  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&time=1-10`, { mode: 'cors' });
-  const img = document.querySelector('img');
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Breakfast&time=1-10&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPIBreakfastMediumPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Breakfast&time=11-20&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPIBreakfastHighPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Breakfast&time=21-60&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPILunchLowPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Lunch&time=1-10&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPILunchMediumPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Lunch&time=11-20&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPILunchHighPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Lunch&time=21-60&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPIDinnerLowPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Dinner&time=1-10&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPIDinnerMediumPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Dinner&time=11-20&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPIDinnerHighPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Dinner&time=21-60&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPISnackLowPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Snack&time=1-10&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPISnackMediumPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Snack&time=11-20&imageSize=REGULAR
+  `, { mode: 'cors' });
+  const recipeSearchData = await response.json();
+  // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
+  console.log(recipeSearchData.hits[0].recipe.calories);
+  generateHTML(recipeSearchData.hits);
+}
+
+async function fetchAPISnackHighPrep() {
+  console.log(searchQuery);
+  const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${searchQuery}&app_id=07219864&app_key=f83320eb349bdebfd72d4be76b29e58f&health=kidney-friendly&mealType=Snack&time=21-60&imageSize=REGULAR
+  `, { mode: 'cors' });
   const recipeSearchData = await response.json();
   // img.src = recipeSearchData.hits[3].recipe.images.thumbnail;
   console.log(recipeSearchData.hits[0].recipe.calories);
